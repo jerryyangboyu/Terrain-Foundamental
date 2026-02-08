2e6fc4d4a34d593e9cdd77cdbd1a92a37b160af4
yangboyu02 <yangboyu02@meituan.com>
2026-02-08 17:48:37 +0800

    feat: perlin noise

--- Assets/Scripts/HeightMapModifiers/NoiseHeightMapModifier.cs
+++ Assets/Scripts/HeightMapModifiers/NoiseHeightMapModifier.cs
@@ -14,26 +14,6 @@ public class NoiseHeightMapModifier : BaseHeightMapModifier
 
     public override void Execute(int mapResolutionSize, float[,] heightMap, Vector3 heightmapScale, byte[,] biomeMap, BiomeConfigSO biomeConfig, int biomeIndex)
     {
-        float currentAmplitude = amplitude;
-        float currentXFrequency = xFrequency;
-        float currentYFrequency = yFrequency;
-
-        for (int pass = 0; pass < octaves; pass++)
-        {
-            for (int y = 0; y < mapResolutionSize; y++)
-            {
-                for (int x = 0; x < mapResolutionSize; x++)
-                {
-                    if (biomeIndex >= 0 && biomeMap[x, y] != biomeIndex) continue;
-                    float noise = Mathf.PerlinNoise(x * currentXFrequency, y * currentYFrequency) * 2f - 1f;
-                    float newHeight = heightMap[y, x] + (noise * currentAmplitude / heightmapScale.y);
-                    heightMap[y, x] = Mathf.Lerp(heightMap[y, x], newHeight, Strength);
-                }
-            }
-
-            currentAmplitude *= persistence;
-            currentXFrequency *= xLacunlarity;
-            currentYFrequency *= yLacunlarity;
-        }
+        // TODO: implement perlin noise based terrain generation algorithm
     }
 }
